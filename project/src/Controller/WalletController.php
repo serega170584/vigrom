661d0b57e46127a64b2488bc16f3adc7f1d45c51
Sergey Krivobokov <serega170584@gmail.com>
2022-05-21 19:20:26 +0300

    service refactoring

--- project/src/Controller/WalletController.php
+++ project/src/Controller/WalletController.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Transaction;
 use App\Entity\Wallet;
+use App\Repository\TransactionRepository;
 use App\Repository\WalletRepository;
-use App\Service\TransactionService;
 use App\Validator\TransactionValidator;
 use App\Validator\WalletValidator;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Doctrine\ORM\OptimisticLockException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,30 +23,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class WalletController extends AbstractController
 {
     /**
-     * @throws \Doctrine\ORM\OptimisticLockException
-     * @throws \Doctrine\ORM\Exception\ORMException
+     * @throws OptimisticLockException
+     * @throws ORMException
      */
     #[Route('/refill/{id}', name: 'wallet_refill', methods: ['POST'])]
     public function refill(
         ?Wallet $wallet,
         Request $request,
         TransactionValidator $transactionValidator,
-        TransactionService $transactionService
+        TransactionRepository $transactionRepository,
+        EntityManager $entityManager
     ): JsonResponse
     {
         $transactionData = $transactionValidator->map($wallet, $request);
 
-        $transactionService->saveTransaction($transactionData);
+        $entity = $transactionRepository->createEntity(Transaction::class);
+        $entity->setWallet($transactionData->getWallet());
+        $entity->setAmount($transactionData->getAmount());
+        $entity->setType($transactionData->getType());
+        $entity->setReason($transactionData->getReason());
+        $entity->setStatus($transactionData->getStatus());
 
-        return $this->json([]);
+        $entityManager->persist($entity);
+        $entityManager->flush();
+
+        return $this->json($entity->getId());
     }
 
     /**
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     * @throws \Doctrine\ORM\NoResultException
+     * @throws NonUniqueResultException
+     * @throws NoResultException
      */
     #[Route('/balance/{id}', name: 'wallet_balance')]
-    public function balance(?Wallet $wallet, WalletValidator $walletValidator, WalletRepository $walletRepository): JsonResponse
+    public function balance(
+        ?Wallet $wallet,
+        WalletValidator $walletValidator,
+        WalletRepository $walletRepository
+    ): JsonResponse
     {
         $walletValidator->validate($wallet);
 
